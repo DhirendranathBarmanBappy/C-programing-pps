5df6d0409696f262e182d86e696d21dd643f56b9
DhirendranathBarmanBappy <114273846+DhirendranathBarmanBappy@users.noreply.github.com>
2022-09-26 00:45:22 +0600

    Add files via upload

---
+++ String/Basic string.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    /*char s1[5];   //akta man besi likte hobe null char ar jonno.noile garvej value asbe.
+
+    s1[0]='A';
+    s1[1]='B';
+    s1[2]='C';
+    s1[3]='D';
+    //s1[4]='\0';
+
+      printf("s1=%s\n",s1);
+*/
+
+//char s1[5]={'A','B','C','D'};  [] aikhane number na dileu hoy.
+//char s1[]="Dhirendranath Barman";
+char s1[]="Dhirendranath Barman \
+                         Bappy";
+printf("S1=%s\n",s1);
+
+
+
+
+}
