38911580fc908885146051105918016fa9152cfc
DhirendranathBarmanBappy <114273846+DhirendranathBarmanBappy@users.noreply.github.com>
2022-09-27 14:16:46 +0600

    Add files via upload

---
+++ String/Concat string with strcat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  char str1[]="My name is ";
+  //char str2[]="Dhirendranath Barman";
+
+  //strcat(str1,str2);
+   strcat(str1,"Bappy Roy");
+
+  printf("str1=%s\n",str1);
+}
